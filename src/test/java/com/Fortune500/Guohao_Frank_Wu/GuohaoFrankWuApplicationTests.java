9a83676c9fb1594ad6a850588eec794004107c1c
Guohao Wu <frankwu1993@gmail.com>
2021-07-23 00:14:41 -0400

    add

---
+++ src/test/java/com/Fortune500/Guohao_Frank_Wu/GuohaoFrankWuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fortune500.Guohao_Frank_Wu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuohaoFrankWuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
